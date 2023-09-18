46f0c289e5b8c5f928e5c54433896577b0ecc5e0
Prayug <prayugsigdel@gmail.com>
2023-09-17 22:31:26 -0700

    changes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Dashboard from "./components/Stocks/Dashboard";
 import StockContext from "./context/StockContext";
@@ -7,10 +7,27 @@ import Signup from "./components/signUp/Signup";
 import { Container } from "react-bootstrap";
 
 function App() {
+  const [showSignup, setShowSignup] = useState(true); // State to control Signup visibility
+  const [darkMode, setDarkMode] = useState(false);
+  const [stockSymbol, setStockSymbol] = useState("MSFT");
+
+  const handleSignupButtonClick = () => {
+    // You can add more logic here if needed
+    // For simplicity, let's just hide the Signup component on button click
+    setShowSignup(false);
+  };
 
   return (
     <Container>
-        <Signup />
+      {showSignup ? (
+        <Signup onSignupButtonClick={handleSignupButtonClick} />
+      ) : (
+        <ThemeContext.Provider value={{ darkMode, setDarkMode }}>
+          <StockContext.Provider value={{ stockSymbol, setStockSymbol }}>
+            <Dashboard />
+          </StockContext.Provider>
+        </ThemeContext.Provider>
+      )}
     </Container>
   );
 }
