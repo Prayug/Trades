8d6634384bf23344ce731f875a43559923b4671e
Prayug <prayugsigdel@gmail.com>
2023-09-19 20:51:38 -0700

    change

--- src/App.js
+++ src/App.js
@@ -18,8 +18,13 @@ function App() {
   };
 
   return (
-    <Container>
-      <Signup onSignupButtonClick={handleSignupButtonClick} />
+    <Container
+      className="d-flex align-items-center justify-content-center"
+      style={{ minHeight: "100vh" }}
+    >
+      <div className="w-100" style={{ maxWidth: "400px" }}>
+        <Signup onSignupButtonClick={handleSignupButtonClick} />
+      </div>
     </Container>
   );
 }
