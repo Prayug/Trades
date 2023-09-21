90074fe78d13df2be2f4759e2f8abafe0fdfaf75
Prayug <prayugsigdel@gmail.com>
2023-09-20 18:39:20 -0700

    Update AuthContext.js

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useContext, useState } from "react";
 
-const AuthContext = () => {
-  return (
-    <div>AuthContext</div>
-  )
+const AuthContext = React.createContext();
+
+export function useAuth() {
+  return useContext(AuthContext);
 }
 
-export default AuthContext
+export function AuthProvider({ children }) {
+
+    const [currentUser, setCurrentUser] = useState();
+
+    const value = {
+        currentUser
+    }
+
+  return <AuthContext.Provider value={value}>
+    {children}
+    </AuthContext.Provider>;
+}
