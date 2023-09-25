27e437aca4c772e8f9e4c9935e0de28f1756d170
Prayug <prayugsigdel@gmail.com>
2023-09-25 10:03:24 -0700

    changes

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ function App() {
 
   return (
     <Container
-      className="d-flex align-items-center justify-content-center left-0"
+      className="d-flex align-items-center justify-content-center"
       style={{ minHeight: "100vh" }}
     >
       <div className="w-100" style={{ maxWidth: "400px" }}>
